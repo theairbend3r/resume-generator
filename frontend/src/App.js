691745f4bae60d70e2f958b1bf30d468268bdbf8
Akshaj Verma <akshajverma7@gmail.com>
2021-09-21 22:54:48 +0530

    Added frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "twin.macro"
+
+function App() {
+  return (
+    <div tw="flex flex-col min-h-screen w-full items-center justify-center text-3xl">
+      <h1>resume generator</h1>
+    </div>
+  )
+}
+
+export default App
