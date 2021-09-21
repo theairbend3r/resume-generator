d2e6f79a679af0a318eabb036ea4aff38138579a
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-09-21 23:02:07 +0530

    server boiler plate done

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/check", (req, res) => {
+  return res.status(200).send({
+    message: "Server up and running succesfully",
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server running on https://localhost:${process.env.PORT || 3000}`
+  );
+});
