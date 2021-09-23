9972ea12fba4953d36d4b9b5ca2ae83aed803bd3
Rose Kamal Love <69139607+rosekamallove@users.noreply.github.com>
2021-09-23 10:54:57 +0530

    create-resume end point

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const pdf = require("html-pdf");
+const pdfTemplate = require("./templates");
+const fs = require("fs");
 
 app.get("/check", (req, res) => {
   return res.status(200).send({
@@ -9,6 +15,17 @@ app.get("/check", (req, res) => {
 
 app.post("/create-resume", (req, res) => {
   console.log(req.body);
+  pdf.create(pdfTemplate(req.body), {}).toStream((err, stream) => {
+    if (err)
+      return res
+        .status(500)
+        .send({ errorMessage: "an error occurred while creating the resume!" });
+    res.writeHead(200, {
+      "Content-Type": "application/force-download",
+      "Content-disposition": "attachment; filename=Resume.pdf",
+    });
+    stream.pipe(res);
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
