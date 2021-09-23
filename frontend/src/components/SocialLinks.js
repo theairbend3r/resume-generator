299ff0317b1e301ff4cead3cdd8f65881867aefe
Akshaj Verma <akshajverma7@gmail.com>
2021-09-23 23:28:15 +0530

    Refactored social links subform.

--- frontend/src/components/SocialLinks.js
+++ frontend/src/components/SocialLinks.js
@@ -1,27 +1,23 @@
-import React from "react";
-import "twin.macro";
-import { AiOutlineLink } from "react-icons/ai";
+import React from "react"
+import "twin.macro"
+import { AiOutlineLink } from "react-icons/ai"
+import FormTitle from "./general/FormTitle"
+import FormField from "./general/FormField"
+import FormContainer from "./general/FormContainer"
 
 function SocialLinks() {
   return (
-    <div tw="px-5 w-full flex flex-col items-center justify-center">
-      <h1 tw="text-3xl mx-4 font-bold flex items-center">
+    <FormContainer>
+      <FormTitle>
         <span tw="mr-4">
-          <AiOutlineLink value={{ className: 'inline-block' }} />
+          <AiOutlineLink value={{ className: "inline-block" }} />
         </span>
         <span>Links</span>
-      </h1>
-      <hr tw="my-3" />
-      <div tw="w-full flex flex-col my-2">
-        <label for="github" tw="text-sm">Github URL</label>
-        <input type="url" tw="p-2 rounded-md w-full border-2" id="github" name="github" />
-      </div>
-      <div tw="w-full flex flex-col my-2">
-        <label for="linkedin" tw="text-sm">LinkedIn URL</label>
-        <input type="url" tw="p-2 rounded-md w-full border-2" id="linkedin" name="linkedin" />
-      </div>
-    </div>
-  );
+      </FormTitle>
+      <FormField field="Github" />
+      <FormField field="LinkedIn" />
+    </FormContainer>
+  )
 }
 
-export default SocialLinks;
+export default SocialLinks
