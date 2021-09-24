a3ccf99b706374d6347970012ef04a4f1d107df3
Meghal Bisht <mailtomeghu25@gmail.com>
2021-09-24 16:54:21 +0530

    Revert "Revert "Refactor home page and basic detail subform.""

--- frontend/src/components/Achievements.js
+++ frontend/src/components/Achievements.js
@@ -1,28 +1,23 @@
-import React from "react";
-import "twin.macro";
-import { AiFillTrophy } from "react-icons/ai";
+import React from "react"
+import "twin.macro"
+import { AiFillTrophy } from "react-icons/ai"
+import FormTitle from "./general/FormTitle"
+import FormContainer from "./general/FormContainer"
+import { TextField } from "./general/FormFields"
 
 function Achievements() {
   return (
-    <div tw="px-5 w-full flex flex-col items-center justify-center">
-      <h1 tw="text-3xl mx-4 font-bold flex items-center">
+    <FormContainer>
+      <FormTitle>
         <span tw="mr-4">
-          <AiFillTrophy value={{ className: 'inline-block' }} />
+          <AiFillTrophy value={{ className: "inline-block" }} />
         </span>
         <span>Achievements</span>
-      </h1>
+      </FormTitle>
       <hr tw="my-3" />
-      <div tw="w-full flex flex-col my-2">
-        <label tw="text-sm" for="achievementDesc">Description</label>
-        <input
-          type="text"
-          tw="p-2 rounded-md w-full border-2"
-          id="achievementDesc"
-          name="achievementDesc"
-        />
-      </div>
-    </div>
-  );
+      <TextField field="Description" />
+    </FormContainer>
+  )
 }
 
-export default Achievements;
+export default Achievements
